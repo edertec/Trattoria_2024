e7789883c3b23239156d39c23c2c172b643b4f82
Ederson Almeida <edertec@msn.com>
2024-09-16 14:08:27 -0300

    config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://default:CETEC@ep-fancy-frog-58641694-pooler.us-east-1.postgres.vercel-storage.com/verceldb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://default:9gx0ktdnaVZI@ep-fancy-frog-58641694-pooler.us-east-1.postgres.vercel-storage.com/verceldb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
